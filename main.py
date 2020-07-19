a205ff6bec3e60de441928d057a13cb95dc9e000
Igor Doico <igordoico@gmail.com>
2020-07-19 18:34:25 -0300

    include ffmpeg

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import youtube_dl
+import pyffmpeg
 
 
 def downloader(url):
-    youtube_dl.YoutubeDL().download([url])
+    url = url.strip()
+    youtube_dl.YoutubeDL(params={
+        'listformats': True,
+        'download_archive': 'output'
+    }).download([url])
+    fm = input('Digite o codigo do melhor formato de audio + melhor formato de video \nExemplo: 137+251\n')
 
+    youtube_dl.YoutubeDL(params={
+        'format': fm,
+        'ffmpeg_location': './ffmpeg/bin/'
+    }).download([url])
 
 if __name__ == '__main__':
     url = input("Cole a URL do youtube\n", )
